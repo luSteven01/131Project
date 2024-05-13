7b2d6f84ceb5cdd3674c476dc92ec3b8e17868d0
luSteven01 <steven.lu01@sjsu.edu>
2024-05-12 21:20:36 -0700

    Here's the most updated I have with the code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 #import flask
-
+#requires use to run flask.Flask(...)
+#from library flask import the class Flask
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+pathName = os.path.abspath(os.path.dirname(__file__))
 
 obj = Flask(__name__)
+
+obj.config.from_mapping(
+    SECRET_KEY='im-batman',
+
+    #where the database file is stored
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(pathName, 'app.db')
+)
+Usersdb =  SQLAlchemy(obj)
 
 from app import routes
